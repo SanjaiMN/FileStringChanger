01371277c1863a7a0d1c5ee4b1d9cb028a3c7611
sanja <sanja@Sanjai_MN>
2023-02-07 23:31:12 +0530

    first commit

---
+++ FileStringchanger/src/StringChanger.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+
+public class StringChanger {
+
+	public static void main(String[] args) 
+	{
+	    try 
+	    {
+	      File file = new File("src\\Filename.txt");
+	      BufferedReader reader = new BufferedReader(new FileReader(file));
+	      Scanner sc = new Scanner(System.in);
+	      String line = "", oldText = "", newText = "A";
+	      System.out.println("Enter the string that needs to be replaced:");
+	      oldText=sc.next();
+	      System.out.println("Enter the new String:");
+	      newText=sc.next();
+	      StringBuilder content = new StringBuilder();
+	      while((line = reader.readLine()) != null) {
+	        content.append(line).append(System.lineSeparator());
+	      }
+
+	      reader.close();
+
+	      String updatedContent = content.toString().replaceAll("\\b"+oldText+"\\b", newText);
+	      System.out.println(updatedContent);
+	      BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+	      writer.write(updatedContent);
+	      writer.close();
+	    }
+	    catch (FileNotFoundException e) 
+	    {
+	        e.printStackTrace();
+	      } catch (IOException e) {
+	        e.printStackTrace();
+	      }
+	}
+}
